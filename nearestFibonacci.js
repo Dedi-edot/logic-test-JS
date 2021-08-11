8ded2f000b1fd403fdf0e7cdc4dc866bcc328ee2
Dedi-edot <id.dedi81@gmail.com>
2021-08-11 12:02:47 +0700

    Edit

--- nearestFibonacci.js
+++ nearestFibonacci.js
@@ -1,4 +1,26 @@
-const numbers = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+const fibonacci = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+
+arr = [15, 1, 3];
+
+function nearestFibonacci(array) {
+  let arr = array;
+  let result = 0;
+  for (let i = 0; i < array.length; i++) {
+    result += array[i];
+  }
+
+  let selisih = [];
+  fibonacci.forEach((val, i) => {
+    selisih[i] = Math.abs(result - val);
+  });
+
+  let nearest = Math.min(...selisih);
+  let terdekat = selisih.indexOf(nearest);
+
+  return fibonacci[terdekat];
+}
+
+console.log(nearestFibonacci(arr));
 
 // function fibonacci(number){
 //     let n1 = 0, n2 = 1, nextTerm;
